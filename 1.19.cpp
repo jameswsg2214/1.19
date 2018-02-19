cabd2790bdab050f9eea115d095799b0a89cca53
jameswsg2214 <36299263+jameswsg2214@users.noreply.github.com>
2018-02-19 17:57:53 +0530

    Add files via upload

---
+++ 1.19.cpp
@@ -0,0 +1,55 @@
+// Example program
+
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+class sample
+{
+    
+public:
+    int c[10];
+
+ int a,i,b;
+    
+int calc()
+{
+    
+for(i=0;i<a;i++)
+    {
+     
+cin>>c[i];
+    
+}
+    
+int v=0;
+   
+ for(i=0;i<b;i++)
+    {
+ 
+      v=v+c[i];
+  
+  }    
+   
+ cout<<"answer is"<<v;
+   
+ return 0;
+
+}
+
+};
+
+int main()
+{
+  
+sample s;
+  
+cin>>s.a;
+ 
+cin>>s.b;
+  
+s.calc();
+
+}
